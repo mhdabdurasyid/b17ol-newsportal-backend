bf5592aa1d2a0f4eebb17f05d4329b81c41c8ebd
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-07 18:00:14 +0700

    add upload user photo profile on update profile

--- src/controllers/usersPrivate.js
+++ src/controllers/usersPrivate.js
@@ -1,4 +1,5 @@
 const responseStandard = require('../helpers/responses')
+const upload = require('../helpers/upload')
 const Joi = require('joi')
 
 const { Users } = require('../models')
@@ -15,35 +16,43 @@ module.exports = {
       return responseStandard(res, 'User not found!', {}, 404, false)
     }
   },
-  updateProfile: async (req, res) => {
+  updateProfile: (req, res) => {
     const { id } = req.user
+    const uploadImage = upload.single('image')
 
     const schema = Joi.object({
       email: Joi.string().email().max(255),
       name: Joi.string().max(255)
     })
 
-    const { error, value } = schema.validate(req.body)
-    const { email, name } = value
-
-    if (email || name) {
-      if (error) {
-        return responseStandard(res, error.message, {}, 400, false)
+    uploadImage(req, res, async (err) => {
+      if (err) {
+        return responseStandard(res, err.message, {}, 400, false)
       } else {
-        const user = await Users.update({
-          email,
-          name
-        }, {
-          where: {
-            id
-          }
-        })
-        console.log(user)
+        const image = req.file
+        const { error, value } = schema.validate(req.body)
+        const { email, name } = value
 
-        return responseStandard(res, 'Update profile successfully!', {})
+        if (email || name || image) {
+          if (error) {
+            return responseStandard(res, error.message, {}, 400, false)
+          } else {
+            await Users.update({
+              email,
+              name,
+              photo: image && `/uploads/${image.filename}`
+            }, {
+              where: {
+                id
+              }
+            })
+
+            return responseStandard(res, 'Update profile successfully!', {})
+          }
+        } else {
+          return responseStandard(res, 'Require one of the input form!', {}, 400, false)
+        }
       }
-    } else {
-      return responseStandard(res, 'Require one of the input form!', {}, 400, false)
-    }
+    })
   }
 }
