5a69f6e7a6f31f9c9386e7f6e34b18b4b218f298
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-07 17:25:18 +0700

    add update user profile

--- src/controllers/usersPrivate.js
+++ src/controllers/usersPrivate.js
@@ -1,4 +1,5 @@
 const responseStandard = require('../helpers/responses')
+const Joi = require('joi')
 
 const { Users } = require('../models')
 
@@ -12,6 +13,37 @@ module.exports = {
       return responseStandard(res, 'Found an user!', { result: user })
     } else {
       return responseStandard(res, 'User not found!', {}, 404, false)
+    }
+  },
+  updateProfile: async (req, res) => {
+    const { id } = req.user
+
+    const schema = Joi.object({
+      email: Joi.string().email().max(255),
+      name: Joi.string().max(255)
+    })
+
+    const { error, value } = schema.validate(req.body)
+    const { email, name } = value
+
+    if (email || name) {
+      if (error) {
+        return responseStandard(res, error.message, {}, 400, false)
+      } else {
+        const user = await Users.update({
+          email,
+          name
+        }, {
+          where: {
+            id
+          }
+        })
+        console.log(user)
+
+        return responseStandard(res, 'Update profile successfully!', {})
+      }
+    } else {
+      return responseStandard(res, 'Require one of the input form!', {}, 400, false)
     }
   }
 }
--- src/routes/usersPrivate.js
+++ src/routes/usersPrivate.js
@@ -4,5 +4,6 @@ const usersController = require('../controllers/usersPrivate')
 const route = Router()
 
 route.get('/', usersController.getUserProfile)
+route.patch('/', usersController.updateProfile)
 
 module.exports = route
