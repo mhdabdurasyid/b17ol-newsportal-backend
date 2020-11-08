baf02f331fa3a771f08ebd2225b5aab28e8c7474
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-08 07:00:25 +0700

    add edit news

--- src/controllers/newsPrivate.js
+++ src/controllers/newsPrivate.js
@@ -28,5 +28,38 @@ module.exports = {
       const result = await news.create(data)
       return responseStandard(res, 'Create news successfully', { result: result })
     }
+  },
+  editNews: async (req, res) => {
+    const { id } = req.user
+    const { newsId } = req.params
+
+    const schema = Joi.object({
+      title: Joi.string().max(255).required(),
+      content: Joi.string().required()
+    })
+
+    const { error, value } = schema.validate(req.body)
+
+    if (error) {
+      return responseStandard(res, error.message, {}, 400, false)
+    } else {
+      const { title, content } = value
+
+      const isEdited = await news.update({
+        title,
+        content
+      }, {
+        where: {
+          id: newsId,
+          author: id
+        }
+      })
+
+      if (isEdited[0] === 1) {
+        return responseStandard(res, 'Edit news successfully!', {})
+      } else {
+        return responseStandard(res, 'Edit news failed!', {}, 400, false)
+      }
+    }
   }
 }
--- src/routes/newsPrivate.js
+++ src/routes/newsPrivate.js
@@ -4,5 +4,6 @@ const newsController = require('../controllers/newsPrivate')
 const route = Router()
 
 route.post('/', newsController.postNews)
+route.put('/:newsId', newsController.editNews)
 
 module.exports = route
