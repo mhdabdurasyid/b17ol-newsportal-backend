66e76d14bcabeca638292b26a903521bf783a45a
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-09 06:48:06 +0700

    drop news_images table

---
+++ src/migrations/20201108233916-drop-news_images-table.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('news_images')
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('news_images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      news_id: {
+        type: Sequelize.INTEGER
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  }
+}
